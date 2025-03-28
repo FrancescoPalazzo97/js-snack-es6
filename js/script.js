7603d716fcce3bcf81629a5235f55b20d4f769ff
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-28 14:23:10 +0100

    [UPD] Defined the object array

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// SNACK 1
+// Definisco l'array di oggetti
+ const bikes = [
+    {
+        name: `Velociter X3`,
+        weight: 8.5,
+    },
+    {
+        name: `Tornado Carbon S1`,
+        weight: 7.2,
+    },
+    {
+        name: `UrbanGlide Pro`,
+        weight: 11.3,
+    },
+    {
+        name: `Alpine Storm MTB`,
+        weight: 14.7,
+    },
+    {
+        name: `EcoRide City`,
+        weight: 9.8,
+    },
+ ]
